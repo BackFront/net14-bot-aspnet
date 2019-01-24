83776fd8d353836f0ac3a30181fadfb0a3ca6462
Douglas Alves <alves.douglaz@gmail.com>
2019-01-24 21:50:22 -0200

    gravando conversa no mogodb

--- Logic/SimpleBotUser.cs
+++ Logic/SimpleBotUser.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,6 +11,10 @@ namespace SimpleBot.Logic
     {
         public string Reply(SimpleMessage message)
         {
+            var cliente = new MongoClient("mongodb://localhost:27017");            var db = cliente.GetDatabase("chatbot");            var col = db.GetCollection<BsonDocument>("log");            col.InsertOne(new BsonDocument() {
+                { "user", message.User },
+                { "message", message.Text }
+            });
             return $"{message.User} disse '{message.Text}";
         }
 
